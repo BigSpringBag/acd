cee00d0d0ec43227c27da5f4d8e93a4714823ce3
Yutao Yan <yutaoyan@Yutaos-MacBook-Pro.local>
2020-08-04 04:48:21 -0400

    change README, added Auth to Schema

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
+
 
 import React from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig);
 
 const App = () => (
   <div>
