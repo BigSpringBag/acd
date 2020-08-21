a327967b381821f8bfc0379fdd10bd120466204b
Yutao Yan <yyan@mars-yyan-mac-64up.lan>
2020-08-21 10:36:18 -0400

    added function to send SNS

--- amplify/backend/function/acdSendSNSLambda/src/index.js
+++ amplify/backend/function/acdSendSNSLambda/src/index.js
@@ -1,10 +1,13 @@
 
 
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+var AWS = require("aws-sdk");
+
+exports.handler = async function(event, context) {
+    var sns = new AWS.SNS();
+    var params = {
+        Message: JSON.stringify(event.message), 
+        Subject: JSON.stringify(event.subject),
+        TopicArn: process.env.SNSTopic
     };
-    return response;
+    await sns.publish(params, context.done);
 };
